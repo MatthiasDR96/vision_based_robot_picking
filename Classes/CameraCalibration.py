4fa6cbf7e5b743054c4ba7ff6422373eb4cbf67b
MatthiasDR96 <matthias.deryck@hotmail.com>
2020-11-01 15:32:52 +0100

    First commit

---
+++ Classes/CameraCalibration.py
@@ -0,0 +1,202 @@
+import glob
+import math
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+# Chessboard pattern
+pattern = (7, 7)
+
+
+# Gives all image path names for images in the file with 'filename'
+def get_image_path_names(filename):
+    return glob.glob(filename)
+
+
+# Get a numpy array from file 'filename'
+def get_numpy_data(filename):
+    return np.load(filename, allow_pickle=True)
+
+
+# Save a numpy array 'data' to a file 'filename'
+def save_to_numpy(filename, data):
+    return np.save(filename, data)
+
+
+# Compute the mean transform from a list of transforms
+def compute_mean_transformation(transformations):
+    return np.mean(transformations, axis=0)
+
+
+# Convert an RGB image to grayscale
+def image_to_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+# Read an RGB image from a file 'filename'
+def read_image(filename):
+    return cv2.imread(filename)
+
+
+# Shows an image
+def show_image(image):
+    cv2.imshow('Image', image)
+    cv2.waitKey(100)
+
+
+# Shows the image with the detected corners of the chessboard
+def show_image_with_chessboard_corners(image, corners):
+    img = cv2.drawChessboardCorners(image, pattern, corners, True)
+    cv2.imshow('Calibration image', img)
+    cv2.waitKey(100)
+
+
+# Load the intrinsic camera matrix and distortion coefficients in a dictionary format form 'filename'
+def load_intrinsic_camera_matrix(filename):
+    intrinsic_camera_matrix = get_numpy_data(filename)
+    data = intrinsic_camera_matrix['data'][()]
+    mtx = data['MTX']
+    dist = data['DIST']
+    return mtx, dist
+
+
+# Multiply two transformation_matrices A and B
+def multiply_transforms(trans_A, trans_B):
+    return np.dot(trans_A, trans_B)
+
+
+# Invert a transformation matrix
+def invert_transform(transform):
+    return np.linalg.inv(transform)
+
+
+# Find the pixel coordinates of the corners of the chessboard in 'image'
+def find_corners(image):
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    found, corners = cv2.findChessboardCorners(image, pattern, None)
+    corners2 = cv2.cornerSubPix(image, corners, (11, 11), (-1, -1), criteria)
+    return corners2
+
+
+# Gives user defined object points for 1 chessboard
+def get_object_points():
+    objp = np.zeros((pattern[0] * pattern[1], 3), np.float32)
+    objp[:, :2] = np.mgrid[0:pattern[0], 0:pattern[1]].T.reshape(-1, 2) * 0.02  # 20x20 mm
+    return objp
+
+
+# Get the intrinsic camera matrix using the point correspondences 'objpoints', and 'imgpoints'
+def calibrate_camera(objpoints, imgpoints):
+    # Read image to get the image shape
+    img = cv2.imread('./data/camera_calibration_images/CC_image_original_1.jpg')
+
+    # To grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Calibrate camera
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    # Intrinsic matrix
+    camera_properties = dict(MTX=mtx, DIST=dist, RVECS=rvecs, TVECS=tvecs)
+
+    # Print matrix
+    print('\nMTX (Intrinsic camera matrix): \n')
+    print(camera_properties['MTX'])
+    print('\nDIST (Distortion coefficients): \n')
+    print(camera_properties['DIST'])
+
+    # Save matrix in numpy format
+    np.savez("./data/matrix_files/intrinsic_camera_properties.npz", data=camera_properties)
+    return camera_properties
+
+
+# Calculate the reprojection error of the intrinsic matrix
+def calculate_reprojection_error(intrinsic_camera_matrix, objpoints, imgpoints):
+    if not intrinsic_camera_matrix:
+        print("No camera matrix found.")
+        exit(0)
+    else:
+        mtx = intrinsic_camera_matrix['MTX']
+        dist = intrinsic_camera_matrix['DIST']
+        rvecs = intrinsic_camera_matrix['RVECS']
+        tvecs = intrinsic_camera_matrix['TVECS']
+
+        tot_error = 0
+        for i in range(len(objpoints)):
+            imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+            error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+            tot_error += error
+        return tot_error
+
+
+# Get the extrinsic camera calibration using the point correspondences and intrinsic camera matrix
+def extrinsic_calibration(frame, objp, corners, mtx, dist):
+    # Extrinsic camera calibration
+    retval, rotation_vector, translation_vector, inliers = cv2.solvePnPRansac(objp, corners, mtx, dist)
+
+    # Rotation_vector into rotation_matrix
+    rvec_matrix = cv2.Rodrigues(rotation_vector)[0]
+
+    # World origin in camera frame
+    ct_transform = data_to_transform(rvec_matrix, translation_vector)
+
+    # Camera's origin in world frame
+    # camera_rotation_matrix = rvec_matrix.T
+    # camera_translation_vector = -np.dot(rvec_matrix.T, translation_vector)
+    # cam_world_transform = data_to_transform(camera_rotation_matrix, camera_translation_vector)
+
+    # Draw and display lines and text on the image
+    draw_show_on_image(frame, rotation_vector, translation_vector, mtx, dist)
+
+    return ct_transform
+
+
+# Convert euler angles to a rotation matrix
+def euler_to_matrix(theta):
+    R_x = np.array([
+        [1, 0, 0],
+        [0, math.cos(theta[0]), -math.sin(theta[0])],
+        [0, math.sin(theta[0]), math.cos(theta[0])]
+    ])
+    R_y = np.array([
+        [math.cos(theta[1]), 0, math.sin(theta[1])],
+        [0, 1, 0],
+        [-math.sin(theta[1]), 0, math.cos(theta[1])]
+    ])
+    R_z = np.array([
+        [math.cos(theta[2]), -math.sin(theta[2]), 0],
+        [math.sin(theta[2]), math.cos(theta[2]), 0],
+        [0, 0, 1]
+    ])
+    R = np.dot(R_z, np.dot(R_y, R_x))
+    return R
+
+
+# Draw the axis of the chessboard frame on the image given world coordinates of the axis
+def draw_show_on_image(frame, rotation_vector, translation_vector, mtx, dist):
+    # Axis coordinates in world coordinates system
+    axis = np.float32([[0.06, 0, 0], [0, 0.06, 0], [0, 0, 0.06], [0, 0, 0]])
+
+    # Project 3D points to image plane
+    axi_imgpts, jacobian = cv2.projectPoints(axis, rotation_vector, translation_vector, mtx, dist)
+
+    cv2.line(frame, tuple(axi_imgpts[3].ravel()), tuple(axi_imgpts[1].ravel()), (0, 255, 0), 2)  # GREEN Y
+    cv2.line(frame, tuple(axi_imgpts[3][0]), tuple(axi_imgpts[2].ravel()), (255, 0, 0), 2)  # BLUE Z
+    cv2.line(frame, tuple(axi_imgpts[3, 0]), tuple(axi_imgpts[0].ravel()), (0, 0, 255), 2)  # RED x
+    text_pos = (axi_imgpts[0].ravel() + np.array([3.5, -7])).astype(int)
+    cv2.putText(frame, 'X', tuple(text_pos), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+    text_pos = (axi_imgpts[1].ravel() + np.array([3.5, -7])).astype(int)
+    cv2.putText(frame, 'Y', tuple(text_pos), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+    text_pos = (axi_imgpts[2].ravel() + np.array([3.5, -7])).astype(int)
+    cv2.putText(frame, 'Z', tuple(text_pos), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+    text_pos = (axi_imgpts[3].ravel() + np.array([200, 50])).astype(int)
+    # cv2.putText(frame, '1unit=2cm', tuple(text_pos), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+
+    # Display the resulting frame
+    plt.imshow(frame)
+
+# Convert a rotation matrix and translation matrix into a transformation matrix
+def data_to_transform(r_matrix, t_position):
+    mat = np.hstack((r_matrix, t_position))
+    mat = np.vstack((mat, [0.0, 0.0, 0.0, 1.0]))
+    return mat
---
+++ Classes/PoseEstimation.py
@@ -0,0 +1,160 @@
+import copy
+import glob
+
+import numpy as np
+import open3d as o3d
+import pcl
+from open3d import PointCloud
+
+voxel_size = 0.01
+max_distance = 0.005
+
+
+# Gives all image path names for images in the file with 'filename'
+def get_image_path_names(filename):
+    return glob.glob(filename)
+
+
+# Get a numpy array from file 'filename'
+def get_numpy_data(filename):
+    return np.load(filename, allow_pickle=True)
+
+
+# Save a numpy array 'data' to a file 'filename'
+def save_to_numpy(filename, data):
+    return np.save(filename, data)
+
+
+# Read a pointcloud from a file 'filename'
+def read_pointcloud(filename):
+    return o3d.read_point_cloud(filename)
+
+
+# Save the pointcloud to a file 'filename'
+def save_pointcloud(pointcloud, filename):
+    pcl.save(pointcloud, filename)
+
+
+# Visualize pointcloud
+def show_pointcloud(pointcloud):
+    o3d.draw_geometries([pointcloud])
+
+
+# Multiply two transformation matrices A and B
+def multiply_transforms(trans_A, trans_B):
+    return np.dot(trans_A, trans_B)
+
+
+# Transform the pointcloud into another coordinate frame using the transformation matrix
+def transform_pointcloud(pointcloud, transform):
+    pointcloud.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    pointcloud.transform(transform)
+    return pointcloud
+
+
+# Covert pointcloud from open3d format to pcl format for plane segmentation
+def pcd_to_pcl_format(pointcloud):
+    o3d.write_point_cloud('data/pointcloud_files/tmp.pcd', pointcloud)
+    return pcl.load('data/pointcloud_files/tmp.pcd')
+
+
+# Convert the transformation matrix to an understandable pose
+def transformation_to_pose(transformation_matrix):
+    trans = (transformation_matrix[0][3], transformation_matrix[1][3], transformation_matrix[2][3])
+    return trans
+
+
+# Filter the pointcloud
+def filter_pointcloud(pointcloud):
+    return o3d.geometry.voxel_down_sample(pointcloud, voxel_size)
+
+
+# Crop the pointcloud (you can play with the bounds)
+def crop_pointcloud(pointcloud):
+    pointcloud_ = np.asanyarray(pointcloud.points)
+    mask_x1 = pointcloud_[:, 0] < 0.3
+    mask_x2 = pointcloud_[:, 0] > -0.3
+    mask_y1 = pointcloud_[:, 1] > -1.0
+    mask_y2 = pointcloud_[:, 1] < -0.5
+    cropped = pointcloud_[np.multiply(np.multiply(mask_x1, mask_x2), np.multiply(mask_y1, mask_y2))]
+    pcd = PointCloud()
+    pcd.points = o3d.Vector3dVector(cropped)
+    return pcd
+
+
+# Segment the table from other objects, inliers = table pointcloud, outliers = objects pointcloud
+def ransac_plane_segmentation(point_cloud, max_distance=max_distance):
+    segmenter = point_cloud.make_segmenter()
+    segmenter.set_model_type(pcl.SACMODEL_PLANE)
+    segmenter.set_method_type(pcl.SAC_RANSAC)
+    segmenter.set_distance_threshold(max_distance)
+    inlier_indices, coefficients = segmenter.segment()
+    inliers = point_cloud.extract(inlier_indices, negative=False)
+    outliers = point_cloud.extract(inlier_indices, negative=True)
+    return inliers, outliers
+
+
+# Read in source and target pointcloud and preprocess
+def prepare_dataset(source, target):
+    print(":: Load two point clouds.")
+    draw_registration_result(source, target, np.identity(4))
+    source_down, source_fpfh = preprocess_point_cloud(source)
+    target_down, target_fpfh = preprocess_point_cloud(target)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+
+# Preprocess pointcloud before registration
+def preprocess_point_cloud(pcd):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = o3d.geometry.voxel_down_sample(pcd, voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    o3d.geometry.estimate_normals(pcd_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.registration.compute_fpfh_feature(pcd_down,
+                                                     o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature,
+                                                                                          max_nn=100))
+    return pcd_down, pcd_fpfh
+
+
+# Do global registration on target and source pointcloud (RANSAC)
+def execute_global_registration(source_down, target_down, source_fpfh, target_fpfh):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, distance_threshold,
+        o3d.registration.TransformationEstimationPointToPoint(False), 4, [
+            o3d.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+            o3d.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.registration.RANSACConvergenceCriteria(4000000, 500))
+    draw_registration_result(source_down, target_down, result.transformation)
+    return result
+
+
+# Do local registration on target and source pointcloud (ICP)
+def refine_registration(source, target, source_fpfh, target_fpfh, result_ransac):
+    distance_threshold = voxel_size * 0.4
+    print(":: Point-to-plane ICP registration is applied on original point")
+    print("   clouds to refine the alignment. This time we use a strict")
+    print("   distance threshold %.3f." % distance_threshold)
+    result = o3d.registration.registration_icp(
+        source, target, distance_threshold, result_ransac.transformation,
+        o3d.registration.TransformationEstimationPointToPlane())
+    draw_registration_result(source, target, result.transformation)
+    return result
+
+
+# Visualize the registration result using two pointclouds and the obtained transformation
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp])
